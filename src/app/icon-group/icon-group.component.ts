17092352dd4906441a696c74e5ebc725bd0a15a0
Abhinand K <abhinandksasi@gmail.com>
2025-05-06 15:35:02 +0530

    feat:navbar added

---
+++ src/app/icon-group/icon-group.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { IconComponent } from '../icons/icons.component';
+
+
+@Component({
+  selector: 'app-icon-group',
+  imports: [IconComponent],
+  templateUrl: './icon-group.component.html',
+  styleUrl: './icon-group.component.css'
+})
+export class IconGroupComponent {
+
+}
