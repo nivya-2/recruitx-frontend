c1685171f24b9902f4e64fe58f0159525cb55fa2
Anjalik712 <anjali2sheeja@gmail.com>
2025-05-06 10:44:45 +0530

    fix: Make dropdown filter component reusable

--- src/app/shared/dropdownfilter/dropdownfilter.component.ts
+++ src/app/shared/dropdownfilter/dropdownfilter.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatFormField } from '@angular/material/select';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon'
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-dropdownfilter',
-  imports: [MatFormField,MatSelectModule,MatFormFieldModule],
+  imports: [MatFormField, MatSelectModule, MatFormFieldModule, MatIconModule ,NgFor],
   templateUrl: './dropdownfilter.component.html',
   styleUrl: './dropdownfilter.component.css'
 })
 export class DropdownfilterComponent {
-
+  @Input() placeholder: string = 'Select an option';
+  @Input() options: string[] = [];
+  
 }
