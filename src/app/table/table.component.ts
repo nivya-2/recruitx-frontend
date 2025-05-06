e959873262561c8b677dd25cd845f503c5d72a84
Jeffin Jitto <jeffin.jitto@experionglobal.local>
2025-05-06 12:45:58 +0530

    feat:pagination

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
 
 
 @Component({
   selector: 'app-table',
-  imports: [MatTableModule,MatSort,CommonModule,MatSortModule],
+  imports: [MatTableModule,MatSort,CommonModule,MatSortModule,MatPaginator],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
@@ -28,15 +30,17 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   @Input() content: any[] = [];
 
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort)  sort!: MatSort;
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
-
+    this.dataSource.paginator = this.paginator;
   }
+  
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource(this.content);
+
   }
 
 
